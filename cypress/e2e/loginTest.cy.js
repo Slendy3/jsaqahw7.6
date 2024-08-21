6ba2048f1924631d82db2bbf3953ffc919b16046
Georgy <balabanov.georgy@mail.com>
2024-08-21 05:25:20 +0300

    Initial commit

---
+++ cypress/e2e/loginTest.cy.js
@@ -0,0 +1,27 @@
+describe("Тесты на авторизацию сайта библиотека", () => {
+  
+  beforeEach(() => {
+    cy.visit("/");
+  })
+  
+  it("Успешная авторизация в библиотеке", () => {
+    cy.login("bropet@mail.ru", "123");
+    cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible").true;
+  })
+
+  it("Ввод пустого поля Email", () => {
+    cy.login(null, "123");
+    cy.get("#mail").then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false;
+      expect(elements[0].validationMessage).to.be.eql("Заполните это поле.");
+    })
+  })
+
+  it("Ввод пустого Password", () => {
+    cy.login("bropet@mail.ru", null);
+    cy.get("#pass").then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false;
+      expect(elements[0].validationMessage).to.be.eql("Заполните это поле.");
+    });
+  });
+})
